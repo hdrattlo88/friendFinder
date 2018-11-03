53c02ffffc72f9a4fd1bac51443cdca16887852a
Heather Drattlo <hdrattlo88@yahoo.com>
2018-11-02 22:37:17 -0700

    update 2--Friend comparison

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,25 +1,52 @@
 
-var friendsData = require("../data/friends.js");
+var path = require('path');
+var friends = require('../data/friends.js');
+var $ = require('jquery');
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+var jsdom = require('jsdom'),
+    { JSDOM } = jsdom,
+    jsdom_options = {
+      runScripts: "dangerously",
+      resources: "usable"
+    };
+var scriptElm = window.document.createElement("script");
+scriptElm.textContent = jQuery;
+DOM.window.document.head.appendChild(scriptElm);
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
 module.exports = function(app) {
- 
-  app.get("/api/friends", function(req, res) {
-    res.json(friendsData);
-  });
+	app.get('/api/friends', function(req, res) {
+		res.json(friends);
+	});
 
-  app.post("/api/friends", function(req, res) {
-      var newFriend = req.body;
-      var newResponses = newFriend.score;
-    
-      friendsData.push(newFriend);
-      res.json(true);
-   
-  });
+	app.post('/api/friends', function(req, res) {
 
-  app.post("/api/clear", function(req, res) {
-    // Empty out the arrays of data
-    friendsData.length = [];
+		var userInput = req.body;
+		var userResponses = userInput.score;
+		var matchName = '';
+		var matchImage = '';
+		var totalDifference = 5; 
 
-    res.json({ ok: true });
-  });
+		for (var i = 0; i < friends.length; i++) {
+			var diff = 0;
+			for (var j = 0; j < userResponses.length; j++) {
+				diff += Math.abs(friends[i].scores[j] - userResponses[j]);
+			}
+
+			if (diff < totalDifference) {
+				console.log('Closest match found = ' + diff);
+			  console.log('Friend name = ' + friends[i].name);
+        console.log('Friend image = ' + friends[i].photo);
+
+				totalDifference = diff;
+				matchName = friends[i].name;
+        matchImage = friends[i].photo;
+        
+        $("#matchName").show('Friend name = ' + friends[i].name);
+        $("#matchPhoto").show(friends[i].photo);
+			}
+		}
+		friends.push(userInput);
+		res.json({status: 'OK', matchName: matchName, matchImage: matchImage});
+	});
 };
